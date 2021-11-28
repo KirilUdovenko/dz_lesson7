3fd53b9d54a4b6c1fadc9597bd5603c2252ae3c0
Kereeshkaa <kiril.suff1@gmail.com>
2021-11-28 15:43:49 +0200

    problems

--- dz_lesson7.py
+++ dz_lesson7.py
@@ -14,29 +14,25 @@ print(some_data)
 
 
 def sort_by_surname(surname_dict):
-    names = surname_dict["name"][-1]
-    return names
+    return surname_dict["name"].split()[-1]
 
 
-data_surname = read_json(filenames)
-data_sort_by_surname = sorted(data_surname, key=lambda surname_dict: len(surname_dict["name"].split()))
+data_sort_by_surname = sorted(some_data, key=lambda surname_dict: surname_dict["name"].split()[-1])
 print(data_sort_by_surname)
 
 
-# def sort_by_years(year_dict):
-#     year = year_dict["years"]
-#     return year
-#
-#
-# data_year = read_json(filenames)
-# data_sort_by_year = re.findall(r"[0-9]+", data_year)
-# print(data_sort_by_year)
+def sort_by_years(year_dict):
+    year = year_dict["years"]
+    return year
+
+
+data_sort_by_year = re.findall(r"\d+", some_data)
+print(data_sort_by_year)
 
 
 def sort_by_len_text(person_dict):
     return len(person_dict["text"].split())
 
 
-data_math = read_json(filenames)
-data_math_sort_by_len_text = sorted(data_math, key=lambda person_dict: len(person_dict["text"].split()))
+data_math_sort_by_len_text = sorted(some_data, key=lambda person_dict: len(person_dict["text"].split()))
 print(data_math_sort_by_len_text)
